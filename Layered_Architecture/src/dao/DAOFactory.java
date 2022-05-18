adcde9280d2dafa2eedb8ca46c094496ca7ffd3d
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-05-19 01:54:04 +0530

    Added Factory design Pattern Test 02

--- Layered_Architecture/src/dao/DAOFactory.java
+++ Layered_Architecture/src/dao/DAOFactory.java
@@ -1,11 +1,13 @@
 package dao;
 
 public class DAOFactory {
+    //singleton design pattern
     private static DAOFactory daoFactory;
 
     private DAOFactory(){
 
     }
+
 
     public static  DAOFactory getDaoFactory(){
         if(daoFactory==null){
@@ -14,8 +16,25 @@ public class DAOFactory {
         return daoFactory;
     }
 
-    public static void test(){
 
+    //factory design pattern
+    public enum DAOTypes{
+        CUSTOMER,ITEM,ORDER,ORDERDETAILS,QUERYDAO
+    }
+
+    public void getDAO(DAOTypes type){
+        switch (type){
+            case CUSTOMER:
+                return;
+            case ITEM:
+                return;
+            case ORDER:
+                return;
+            case ORDERDETAILS:
+                return;
+            case QUERYDAO:
+                return;
+        }
     }
 
 
--- Layered_Architecture/src/dao/Test.java
+++ Layered_Architecture/src/dao/Test.java
@@ -3,7 +3,7 @@ package dao;
 public class Test {
     public static void main(String[] args) {
         DAOFactory daoFactory = DAOFactory.getDaoFactory();
-        daoFactory.test();
+        daoFactory.getDAO(DAOFactory.DAOTypes.ITEM);
 
     }
 }
